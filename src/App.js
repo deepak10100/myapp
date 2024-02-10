09e22c746164ca84c86b0010429896462cd411d1
deepak <deepakmalakar98185@gmail.com>
2024-02-10 23:19:50 +0530

    10 day

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Home from "./components/Home";
+import React,{ createContext} from 'react'
 
-
-
+let userContext = createContext()
 function App() {
-
+  // const [user, setUser] = useState("deepak");
   return (
 <>
- <Home/>
+
+ <Home />
+
 </>
   );
 }
 
 export default App;
+export {userContext}
