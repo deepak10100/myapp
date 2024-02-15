52b5d588fac62b15e4374ae63e47748d5c1a647b
deepak <deepakmalakar98185@gmail.com>
2024-02-16 00:29:45 +0530

    16 day

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import React,{useContext, useEffect, useState} from 'react'
 import { userContext } from '../App'
+import axios from 'axios';
 export default function Home() {
 const [count ,setCount] =  useState(0)
+const [count1 ,setCount1] =  useState()
+
+useEffect(() => {
+ async function getData() {
+    const res = await axios.get(' https://pokeapi.co/api/v2/pokemon/')
+    setCount1(res.data.results[0].url)
+ }
+ getData()
+});
 let minusHandler = ()=>{
   setCount(count-1)
 }
@@ -16,6 +26,8 @@ useEffect(() => {
 
   return (
     <>
+    <p>{count1}</p>
+    <a href= {count1} >link</a>
     <input type="number" readOnly disabled  value={count}  name="" id=""/>
     <button type="submit" onClick={minusHandler}>Minus</button>
     <button type="submit" onClick={addHandler}>Add</button>
